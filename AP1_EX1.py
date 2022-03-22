d1a45c3e41a85e6831e1f3c2be756b3fff6b1d61
Marileide Martins Alves <68040258+MarileideMartinsAlves@users.noreply.github.com>
2022-03-22 20:39:29 -0300

    Update AP1_EX1.py

--- AP1_EX1.py
+++ AP1_EX1.py
@@ -1,10 +1,9 @@
 #  Atividade Prática Lógica de Programação e Algoritmos ** Exercício 1 **
 
 print('Bem Vindo(a) à Loja da Marileide Martins Alves') # identificador pessoal
-valorproduto3049987 = float(input('Digite o valor do produto: '))  # valor unitário do produto do tipo flutuante
-# RU na variável
+valorproduto = float(input('Digite o valor do produto: '))  # valor unitário do produto do tipo flutuante
 quantidadeproduto = int(input('Digite a quantidade do produto: '))  # quantidade de produto do tipo inteiro
-comprasemdesconto = float(valorproduto3049987*quantidadeproduto)  # cálculo da compra sem desconto
+comprasemdesconto = float(valorproduto*quantidadeproduto)  # cálculo da compra sem desconto
 
 if 10 <= quantidadeproduto <= 99:  # if contemplando a primeira condição para receber desconto
     desconto = float(0.05)  # desconto que precisa ser aplicado ao atender a condição acima
